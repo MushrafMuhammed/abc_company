5eaa1f04a6f10a201988316ca2c12e4521e0239a
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2022-10-18 14:09:09 +0530

    img_2 commit

--- company_admin/urls.py
+++ company_admin/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path,include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     
     path('login', views.loginfun, name='login'),
     path('signup',views.signupfun, name='signup')
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
