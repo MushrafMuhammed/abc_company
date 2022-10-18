9632ddb39ad781146724642fcf89e11c11795c1b
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2022-10-18 13:15:25 +0530

    heroku host_commit

--- abc_company/settings.py
+++ abc_company/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xyxcidw0!$3%h7hm29d$n2x0=-bbk#oi!lumv801p^gswwbre6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
